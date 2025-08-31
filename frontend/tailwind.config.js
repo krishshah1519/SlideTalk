4760db31e7174aa2f05cf5059f2642a68ef65aa1
krishshah1519 <krishshah1519@gmail.com>
2025-08-31 11:46:19 +0530

    initialized tailwind.css

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // This includes all your components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
